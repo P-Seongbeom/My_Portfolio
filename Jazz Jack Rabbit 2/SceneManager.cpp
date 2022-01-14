ed70f54d4a4dcb11cf2d32418e8a969171e88301
P-Seongbeom <psb0929@gmail.com>
2022-01-14 11:07:34 +0900

    feat : shooting motion, fix : player motion animation

--- Jazz Jack Rabbit 2/SceneManager.cpp
+++ Jazz Jack Rabbit 2/SceneManager.cpp
@@ -28,7 +28,7 @@ void SceneManager::Init()
 	SceneManager::GetSingleton()->AddScene("TiltleScene", new TiltleScene);
 	SceneManager::GetSingleton()->AddScene("PlayInStage", new PlayInStageScene);
 
-	SceneManager::GetSingleton()->ChangeScene("PlayInStage");
+	SceneManager::GetSingleton()->ChangeScene("TiltleScene");
 }
 
 void SceneManager::Release()
